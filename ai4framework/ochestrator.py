582ff02e81837b5bf086a18b66b5ad7ece94ceb1
abderrahmanyouabd <youabd50@gmail.com>
2024-08-26 15:10:18 +0200

    minor modification

--- ai4framework/ochestrator.py
+++ ai4framework/ochestrator.py
@@ -2,27 +2,28 @@ from core.security_classifier import SecurityClassifier
 from core.symbolic_execution import SymbolicExecution
 from config.common_config import ConfigManager
 from utils.logger import logger
+import sast.sast_orchestrator
 
 class WorkflowFramework:
     def __init__(self):
         self.config = ConfigManager.get_config()
-        # self.sast = sast.sast_orchestrator.SASTOrchestrator(self.config)
+        self.sast = sast.sast_orchestrator.SASTOrchestrator(self.config)
 
         # self.security_classifier = SecurityClassifier(self.config)
         # self.security_annotator = SecurityAnnotator(self.config)
         # self.fix_generator = FixGenerator(self.config)
 
-        self.symbolic_execution = SymbolicExecution(self.config)
+        # self.symbolic_execution = SymbolicExecution(self.config)
 
     def execute_workflow(self):
         logger.info("Starting workflow execution")
-        # self.sast.run_all()
+        self.sast.run_all()
         # classified_code = self.security_classifier.classify(project_path)
         # annotated_code = self.security_annotator.annotate(classified_code)
         # issues_file = self.symbolic_executor.execute(annotated_code)
 
         # self.sast_engine.analyze()
-        self.symbolic_execution.analyze()
+        # self.symbolic_execution.analyze()
 
         logger.info("Workflow execution completed")
         pass
