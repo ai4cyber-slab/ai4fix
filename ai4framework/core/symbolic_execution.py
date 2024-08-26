99f6d98eb35722fceda7f40f55eedb82cd99575d
Abderrahman Youabd <youabd50@gmail.com>
2024-08-26 14:45:01 +0200

    updated symbolic execution

--- ai4framework/core/symbolic_execution.py
+++ ai4framework/core/symbolic_execution.py
@@ -5,22 +5,24 @@ from utils.logger import logger
 class SymbolicExecution:
     def __init__(self, config):
         self.config = config
+        self.project_name = self.config.get("DEFAULT", "config.project_name")
+        self.project_path = self.config.get("DEFAULT", "config.project_path")
+        self.results_path = self.config.get("ANALYZER", "config.analyzer_results_path")
+        self.analyzer = self.config.get("ANALYZER", "config.analyzer")
     
-    def analyze(self, project):
-        logger.info(f"Analyzing project: {project}")
-        
+    def analyze(self):
+        logger.info(f"Analyzing project: {self.project_name}")
+
         commands = [
-            f'./Java/AnalyzerJava -projectName={project} -projectBaseDir={project} -resultsDir=Results -currentDate=now -runFB=false -runAndroidHunter=false -runMetricHunter=false -runDCF=false -runMET=false -runVulnerabilityHunter=false -runMET=false -runLIM2Patterns=false -runFaultHunter=false -runPMD=false',
-            f'./Java/LinuxTools/GraphDump ./Results/{project}/java/now/analyzer/graph/{project}-RTEHunter.graph -json:./Results/{project}/java/now/analyzer/graph/{project}-RTEHunter.json'
-        ]
+        f'{self.analyzer} -projectName={self.project_name} -projectBaseDir={self.project_path} -resultsDir={self.results_path} -currentDate=now -runFB=false -runAndroidHunter=false -runMetricHunter=false -runDCF=false -runMET=false -runVulnerabilityHunter=false -runMET=false -runLIM2Patterns=false -runFaultHunter=false -runPMD=false',
+        f'cp {self.results_path}/{self.project_name}/java/now/{self.project_name}-RTEHunter.txt {self.results_path}/{self.project_name}/java/now/{self.project_name}-RTEHunter.json'
+]
 
         command_string = ' && '.join(commands)
-        arch_cwd = '/mnt/c/Users/HP/Music/demo'
         
         try:
             process = subprocess.Popen(
                 command_string,
-                cwd=arch_cwd,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 text=True,
@@ -33,7 +35,6 @@ class SymbolicExecution:
                 if output == '' and process.poll() is not None:
                     break
                 if output.strip():
-                    # Remove the timestamp from the output line
                     clean_output = re.sub(r'^\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] ', '', output.strip())
                     logger.info(clean_output)
 
@@ -41,13 +42,8 @@ class SymbolicExecution:
             if stderr_output:
                 for line in stderr_output.splitlines():
                     if line.strip():
-                        # Remove the timestamp from the error line
                         clean_error = re.sub(r'^\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] ', '', output.strip())
                         logger.error(clean_error)
 
         except Exception as e:
             logger.error(f"An error occurred: {e}")
-
-# if __name__ == '__main__':
-#     symbolic_execution = SymbolicExecution({})
-#     symbolic_execution.analyze('Demo')
