8f11ada89e05f09a2aafc83cb47e0d103ed3c626
Abderrahman Youabd <youabd50@gmail.com>
2024-09-15 09:56:53 +0200

    Updated configuration files and added documentation to config folder

--- ai4framework/core/patch_applier.py
+++
@@ -1,9 +0,0 @@
-from utils.logger import logger
-
-class PatchApplier:
-    def __init__(self, config):
-        self.config = config
-
-    def apply_patches(self, patch_file):
-        logger.info("Applying patches")
-        # check patch.ts for insights
