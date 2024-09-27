4a99bf6d355ac0760ad0ea6ba5214f8eadf5787e
Abderrahman Youabd <youabd50@gmail.com>
2024-09-27 14:57:36 +0200

    added minor changes

--- ai4framework/utils/vscode_refresh.py
+++ ai4framework/utils/vscode_refresh.py
@@ -3,9 +3,14 @@ from utils.logger import logger
 import time
 
 def reload_vscode_window(config):
+    flag = 0
     try:
-        logger.warning('Please note that you should use the newly opened window for the settings to take effect.')
+        logger.warning('Please note that you should use the newly opened vscode window for the settings to take effect.')
         time.sleep(2)
         subprocess.run(["code-server", config.get('DEFAULT', 'config.project_path'), "--reload-window"], check=True)
+        flag = 1
+
     except subprocess.CalledProcessError as e:
-        logger.error(f"Failed to reload VSCode window: {e}")
+        logger.warning(e)
+        if flag == 0:
+            print(f"Please re-run the previous Docker command without the 'bash' suffix to open VS Code in your browser. Once it's running, navigate to: http://localhost:8080/?folder={config.get('DEFAULT', 'config.project_path')}")
